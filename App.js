6c1dd63ad760cd438a83913e5301b0883144528c
DESKTOP-0MNRU9B\Kenneth <knnthcrtl.kc@gmail.com>
2023-05-15 00:15:47 +0800

    setup react expo with nativebase

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NativeBaseProvider, Box, Text } from "native-base";
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box>
+    </NativeBaseProvider>
+  );
+}
