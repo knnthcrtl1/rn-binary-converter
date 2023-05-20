7d3c4840548104dc83e5987e8db2266976f48643
DESKTOP-0MNRU9B\Kenneth <knnthcrtl.kc@gmail.com>
2023-05-21 00:39:11 +0800

    remove console log

--- src/services/converter.ts
+++ src/services/converter.ts
@@ -4,7 +4,6 @@ const useConverter = () => {
   const [result, setResult] = React.useState('')
 
   const convertBinary = (from: string, to: string, bin: string) => {
-    console.log(from, to)
     if (from === '1') {
       const strNumber = to === '2' ? undefined : to === '3' ? 8 : 16
       const digit = parseInt(bin, 2).toString(strNumber)
